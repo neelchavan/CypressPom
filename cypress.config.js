9d6c832de0030023327a871849a0be9a43477913
neelchavan <neelchavan9@gmail.com>
2023-04-18 16:58:03 +0530

    added mocha-awesome reporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter", // for html reports
   env: {
     baseUrl: "https://rahulshettyacademy.com/angularpractice/",
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on); // for html reports
     },
     specPattern: "cypress/integration/Tests/*.js",
     downloadsFolder: "cypress/downloads",
